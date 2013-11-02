66557a6a44ff39e8d6bfeb0083d592082a0a1eef
Erik Ylvisaker <eylvisaker@gmail.com>
2013-11-02 15:48:46 +0000

    Actually add the tests to the repository this time.
    git-svn-id: svn://192.168.0.200/Xle/trunk@110 8e2493a0-6f32-3e4d-a2e5-9abab0ebc7f0

---
+++ XleTests/MuseumExhibitTests.cs
@@ -0,0 +1,82 @@
+﻿using ERY.Xle;
+using ERY.Xle.XleMapTypes.MuseumDisplays;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERY.XleTests
+{
+	[TestClass]
+	public class MuseumExhibitTests
+	{
+		[TestMethod]
+		public void InformationLevelupTest()
+		{
+			Player player = new Player();
+			player.ItemCount(15, -1); // remove the compendium the player started with.
+			
+			Information information = new Information();
+
+			Assert.IsFalse(information.ShouldLevelUp(player));
+
+			player.museum[(int)ExhibitIdentifier.Thornberry] = 1;
+			player.museum[(int)ExhibitIdentifier.Weaponry] = 10; // mark weaponry as closed.
+			player.museum[(int)ExhibitIdentifier.Fountain] = 1;
+
+			Assert.IsTrue(information.ShouldLevelUp(player));
+
+			player.Level = 2;
+			Assert.IsFalse(information.ShouldLevelUp(player));
+
+			player.museum[(int)ExhibitIdentifier.NativeCurrency] = 1;
+			player.museum[(int)ExhibitIdentifier.HerbOfLife] = 1;
+			player.museum[(int)ExhibitIdentifier.PirateTreasure] = 1;
+
+			Assert.IsFalse(information.ShouldLevelUp(player));
+			player.Variables["BeenInDungeon"] = 1;
+			Assert.IsTrue(information.ShouldLevelUp(player));
+
+			player.Level = 3;
+			Assert.IsFalse(information.ShouldLevelUp(player));
+
+			player.museum[(int)ExhibitIdentifier.StonesWisdom] = 1;
+			player.museum[(int)ExhibitIdentifier.Tapestry] = 1;
+			player.museum[(int)ExhibitIdentifier.LostDisplays] = 1;
+
+			Assert.IsTrue(information.ShouldLevelUp(player));
+
+			player.Level = 4;
+			Assert.IsFalse(information.ShouldLevelUp(player));
+
+			player.museum[(int)ExhibitIdentifier.KnightsTest] = 1;
+			player.ItemCount(12, 1); // give magic ice
+
+			Assert.IsTrue(information.ShouldLevelUp(player));
+
+			player.Level = 5;
+			Assert.IsFalse(information.ShouldLevelUp(player));
+
+			player.Variables["Guardian"] = 3;
+			Assert.IsTrue(information.ShouldLevelUp(player));
+
+			player.Level = 6;
+
+			for (int i = 0; i < 4; i++)
+			{
+				Assert.IsFalse(information.ShouldLevelUp(player));
+				player.ItemCount(14, 1); //  add a guard jewel.
+			}
+			Assert.IsTrue(information.ShouldLevelUp(player));
+
+			player.Level = 7;
+			Assert.IsFalse(information.ShouldLevelUp(player));
+
+			player.ItemCount(15, 1); // give the compendium
+			Assert.IsTrue(information.ShouldLevelUp(player));
+
+		}
+	}
+}
---
+++ XleTests/XleMapTests.cs
@@ -0,0 +1,96 @@
+﻿using ERY.Xle;
+using ERY.Xle.XleEventTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERY.XleTests
+{
+	class FakeMap : XleMap
+	{
+		public override void InitializeMap(int width, int height)
+		{
+			throw new NotImplementedException();
+		}
+		public override int Width
+		{
+			get { throw new NotImplementedException(); }
+		}
+		public override int Height
+		{
+			get { throw new NotImplementedException(); }
+		}
+		public override int this[int xx, int yy]
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+		protected override void DrawImpl(int x, int y, Direction faceDirection, AgateLib.Geometry.Rectangle inRect)
+		{
+			throw new NotImplementedException();
+		}
+		public override void GetBoxColors(out AgateLib.Geometry.Color boxColor, out AgateLib.Geometry.Color innerColor, out AgateLib.Geometry.Color fontColor, out int vertLine)
+		{
+			throw new NotImplementedException();
+		}
+		public override string[] MapMenu()
+		{
+			throw new NotImplementedException();
+		}
+		protected override bool CheckMovementImpl(Player player, int dx, int dy)
+		{
+			throw new NotImplementedException();
+		}
+		public override bool CanPlayerStepInto(Player player, int xx, int yy)
+		{
+			throw new NotImplementedException();
+		}
+		public override void PlayerCursorMovement(Player player, Direction dir)
+		{
+			throw new NotImplementedException();
+		}
+		public override bool PlayerFight(Player player)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	[TestClass]
+	public class XleMapTests
+	{
+		[TestMethod]
+		public void HasEventTypeTest()
+		{
+			XleMap map = new FakeMap();
+
+			bool threwException = false;
+			try
+			{
+				map.HasEventType(typeof(int));
+			}
+			catch (ArgumentException e)
+			{
+				threwException = true;
+			}
+			Assert.IsTrue(threwException);
+
+			map.Events.Add(new StoreBank());
+
+			Assert.IsTrue(map.HasEventType<StoreBank>());
+			Assert.IsTrue(map.HasEventType(typeof(StoreBank)));
+
+			Assert.IsFalse(map.HasEventType<StoreArmor>());
+			Assert.IsFalse(map.HasEventType(typeof(StoreArmor)));
+
+		}
+	}
+}
