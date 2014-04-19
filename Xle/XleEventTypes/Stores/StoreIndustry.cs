153276db30406c7d30c861b60d48d0c811188042
Erik Ylvisaker <eylvisaker@gmail.com>
2014-04-19 02:12:27 +0000

    Add events to blacksilver towns.
    git-svn-id: svn://192.168.0.200/Xle/trunk@362 8e2493a0-6f32-3e4d-a2e5-9abab0ebc7f0

---
+++ Xle/XleEventTypes/Stores/StoreIndustry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ERY.Xle.XleEventTypes.Stores
+{
+	public class StoreIndustry : Store
+	{
+		protected override void ReadData(AgateLib.Serialization.Xle.XleSerializationInfo info)
+		{
+			base.ReadData(info);
+
+			Product = info.ReadString("Product");
+		}
+		protected override void WriteData(AgateLib.Serialization.Xle.XleSerializationInfo info)
+		{
+			base.WriteData(info);
+
+			info.Write("Product", Product);
+		}
+
+		public string Product { get; set; }
+	}
+}
