1c6808d43d43caad31340140350754c8593b127e
Erik Ylvisaker <eylvisaker@gmail.com>
2015-06-07 10:28:00 -0700

    Fix TestCoverage.bat

--- TestCoverage.bat
+++ TestCoverage.bat
@@ -1,1 +1,1 @@
-OpenCover.Console.exe -target:C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\MSTest.exe -targetargs:/testcontainer:Zodiac.UnitTests\bin\Release\Zodiac.UnitTests.dll -targetargs:/testcontainer:XleTests\bin\Debug\XleTests.dll -targetargs:/noisolation -output:testcoverage.xml
+OpenCover.Console.exe "-target:C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\MSTest.exe" -targetargs:/testcontainer:XleTests\bin\Debug\XleTests.dll -targetargs:/noisolation -output:testcoverage.xml
