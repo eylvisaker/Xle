69cbcd51e7cccd3c602c93a0be3be6ab33d2bb05
Erik Ylvisaker <eylvisaker@gmail.com>
2015-06-07 11:45:19 -0700

    Fix unit test script

--- UnitTest.bat
+++ UnitTest.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\MSTest.exe" /testcontainer:XleTest\bin\Debug\XleTest.dll 
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\MSTest.exe" /testcontainer:XleTests\bin\Debug\XleTests.dll 
