2d8f7970e59665500c53ebddc9514802d5c3239e
Erik Ylvisaker <eylvisaker@gmail.com>
2015-06-29 17:54:41 -0700

    Add missing LotaTest file.

---
+++ XleTests/LegacyOfTheAncients/LotaTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ERY.XleTests.LegacyOfTheAncients
+{
+    [TestClass]
+    public class LotaTest : XleTest
+    {
+    }
+}
