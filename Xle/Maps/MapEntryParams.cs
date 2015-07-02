f5a213823e232846805cdafe0684cdcf6bf2436b
Erik Ylvisaker <eylvisaker@gmail.com>
2015-07-02 08:23:38 -0700

    Add missing MapEntryParams file.

---
+++ Xle/Maps/MapEntryParams.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ERY.Xle.Maps
+{
+    public class MapEntryParams
+    {
+        public int EntryPoint { get; set; }
+    }
+}
