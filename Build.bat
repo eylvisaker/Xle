5147434a86159b4acc5f27eecfdb14a465e7b55c
Erik Ylvisaker <eylvisaker@gmail.com>
2015-07-26 16:49:57 -0700

    Update build scripts.

--- Build.bat
+++ Build.bat
@@ -1,3 +1,12 @@
 @ECHO OFF
 
+nuget restore
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
+cd Agate
+nuget restore
+if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
+
+cd ..
+
 msbuild.exe Xle.sln /T:rebuild /P:Configuration=%1
