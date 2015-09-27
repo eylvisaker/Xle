963a0e0cf47152b71113c6c4809e2531132b793c
Erik Ylvisaker <eylvisaker@gmail.com>
2015-09-27 13:33:25 -0700

    Update unit test script removing AgateLib reference.

--- UnitTest.bat
+++ UnitTest.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\MSTest.exe" /testcontainer:XleTests\bin\Debug\XleTests.dll /testcontainer:Agate\AgateLib.Tests\UnitTests\bin\Debug\AgateLib.UnitTests.dll
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\MSTest.exe" /testcontainer:XleTests\bin\Debug\XleTests.dll 
