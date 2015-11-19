b521fe541a85406167beeabf5f2b5557c6f6419d
Erik Ylvisaker <eylvisaker@gmail.com>
2015-11-19 07:14:34 -0800

    Remove AgateLib build from build.bat.

--- Build.bat
+++ Build.bat
@@ -3,10 +3,4 @@
 nuget restore Xle.sln
 if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
 
-cd Agate
-nuget restore AgateLib-Windows.sln
-if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
-
-cd ..
-
 msbuild.exe Xle.sln /T:rebuild /P:Configuration=%1
