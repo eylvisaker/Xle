21feda38b0e00f0127737f86cc499e51a3bbf83a
Erik Ylvisaker <eylvisaker@gmail.com>
2017-01-08 22:47:46 -0800

    Add -register:user switch to coverage.bat

--- Coverage.bat
+++ Coverage.bat
@@ -1,1 +1,1 @@
-OpenCover.Console.exe -target:MSTest.exe -targetargs:/testcontainer:XleTests\bin\Debug\XleTests.dll -targetargs:/noisolation -excludebyfile:*\*.Designer.cs -output:Coverage.xml
+OpenCover.Console.exe -target:MSTest.exe -targetargs:/testcontainer:XleTests\bin\Debug\XleTests.dll -targetargs:/noisolation -excludebyfile:*\*.Designer.cs -output:Coverage.xml -register:user
