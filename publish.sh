a99ddca9a14cf4c020b611dbd5f7c47b901f9c66
Erik Ylvisaker <eylvisaker@gmail.com>
2018-09-30 01:28:39 +0000

    Fix construction of destination directory in publish script if  is empty

--- publish.sh
+++ publish.sh
@@ -5,8 +5,16 @@ remoteHost=$FTP_REMOTE_HOST
 username=$FTP_USERNAME
 password=$FTP_PASSWORD
 
+lotadest="LegacyOfTheAncients"
+lobdest="LegendOfBlacksilver"
+
+if [[ ! -z $FTP_DEST ]]; then
+  lotadest=$FTP_DEST/$lotadest
+  lobdest=$FTP_DEST/$lobdest
+fi
+
 echo "Uploading to ftp://$FTP_REMOTE_HOST/$dest"
 echo "Logging in as $FTP_USERNAME"
 
-ftp-upload -h $FTP_REMOTE_HOST -u $FTP_USERNAME --password $FTP_PASSWORD -d $FTP_DEST/LegacyOfTheAncients $1/LegacyOfTheAncients*.zip
-ftp-upload -h $FTP_REMOTE_HOST -u $FTP_USERNAME --password $FTP_PASSWORD -d $FTP_DEST/LegendOfBlacksilver$1/LegendOfBlacksilver*.zip
+ftp-upload -h $FTP_REMOTE_HOST -u $FTP_USERNAME --password $FTP_PASSWORD -d $lotadest $1/LegacyOfTheAncients*.zip
+ftp-upload -h $FTP_REMOTE_HOST -u $FTP_USERNAME --password $FTP_PASSWORD -d $lobdest $1/LegendOfBlacksilver*.zip
